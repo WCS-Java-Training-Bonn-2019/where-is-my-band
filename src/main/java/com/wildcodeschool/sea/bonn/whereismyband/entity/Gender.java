342e9738a7309234ed23652fc20bc3b34cb60bf2
mkrill <michael.krill@telekom.de>
2020-01-22 12:56:19 +0100

    cleaned up and initialized project for sprint 2 with musician and gender as a basis

--- src/main/java/com/wildcodeschool/sea/bonn/whereismyband/entity/Gender.java
+++ src/main/java/com/wildcodeschool/sea/bonn/whereismyband/entity/Gender.java
@@ -1,31 +1,33 @@
 package com.wildcodeschool.sea.bonn.whereismyband.entity;
 
-public class Gender {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-	// database id
+@Entity
+public class Gender {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-
-	public Gender() {
-	}
-
-	public Gender(Long id, String name) {
-		super();
-		this.id = id;
-		this.name = name;
-	}
 
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 
 }
