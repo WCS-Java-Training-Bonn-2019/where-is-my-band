dfe6e152873d39a39b2c0fdb502a9f8cc9698d3e
marcelkamp1 <marcelkamp1@gmx.de>
2020-01-31 09:57:39 +0100

    dynamische seite lauffähig

--- src/main/java/com/wildcodeschool/sea/bonn/whereismyband/controller/SearchController.java
+++ src/main/java/com/wildcodeschool/sea/bonn/whereismyband/controller/SearchController.java
@@ -6,9 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.wildcodeschool.sea.bonn.whereismyband.repository.AddressRepository;
+import com.wildcodeschool.sea.bonn.whereismyband.entity.PositionState;
 import com.wildcodeschool.sea.bonn.whereismyband.repository.BandRepository;
-import com.wildcodeschool.sea.bonn.whereismyband.repository.BandpositionRepository;
 import com.wildcodeschool.sea.bonn.whereismyband.repository.GenreRepository;
 import com.wildcodeschool.sea.bonn.whereismyband.repository.InstrumentRepository;
 
@@ -16,37 +15,38 @@ import com.wildcodeschool.sea.bonn.whereismyband.repository.InstrumentRepository
 @RequestMapping("/search")
 public class SearchController {
 
-	private BandpositionRepository bandpositionRepository;
 	private GenreRepository genreRepository;
 	private InstrumentRepository instrumentRepository;
-	private AddressRepository addressRepository;
 	private BandRepository bandRepository;
 	
 	@Autowired
 	public SearchController(
-			BandpositionRepository bandpositionRepository,
-			AddressRepository addressRepository,
 		 	BandRepository bandRepository,
 			GenreRepository genreRepository,
 			InstrumentRepository instrumentRepository) {
 		
 		super();
-		this.bandpositionRepository = bandpositionRepository;
-		this.addressRepository = addressRepository;
 		this.bandRepository = bandRepository;
 		this.genreRepository = genreRepository;
 		this.instrumentRepository = instrumentRepository;
 	}
 	
-	@GetMapping("list")
+	@GetMapping("list/open")
+	public String getOpen(Model model) {
+		
+		model.addAttribute("bands", bandRepository.findDistinctByBandPositionsState(PositionState.offen));
+		model.addAttribute("instruments", instrumentRepository.findAll());
+		model.addAttribute("genres", genreRepository.findAll());
+		return "bandsuche-template-v2";
+	}
+	
+	@GetMapping("list/all")
 	public String getAll(Model model) {
 		
 		model.addAttribute("bands", bandRepository.findAll());
-		model.addAttribute("bandpositions", bandpositionRepository.findAll());
-		model.addAttribute("addresses", addressRepository.findAll());
 		model.addAttribute("instruments", instrumentRepository.findAll());
 		model.addAttribute("genres", genreRepository.findAll());
-		return "bandsuche_dynamisch";
+		return "bandsuche-template-v2";
 	}
 	
 }
--- src/main/java/com/wildcodeschool/sea/bonn/whereismyband/repository/BandRepository.java
+++ src/main/java/com/wildcodeschool/sea/bonn/whereismyband/repository/BandRepository.java
@@ -1,19 +1,21 @@
 package com.wildcodeschool.sea.bonn.whereismyband.repository;
 
 
+import java.util.List;
 import java.util.Optional;
-
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.wildcodeschool.sea.bonn.whereismyband.entity.Band;
+import com.wildcodeschool.sea.bonn.whereismyband.entity.PositionState;
 
 @Repository
 public interface BandRepository extends JpaRepository<Band, Long>{
 	
 
 	public Optional<Band> findByName(String name);
+	public List<Band> findDistinctByBandPositionsState(PositionState state);
 
 
 }
